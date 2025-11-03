c8ba3791844f4d6be8b4605a0a96ca3ac4533481
javadzamanifard <javadzamanii.1374@gmail.com>
2025-11-03 06:36:04 -0800

    feat:Implement header tepmlate and improve courselist view, home/courselist templates

--- courses/views.py
+++ courses/views.py
@@ -9,6 +9,16 @@ class CourseListView(generic.ListView):
     template_name = 'courses/course_list.html'
     context_object_name = 'courses'
     
-    
     def get_queryset(self):
+        course_type = self.request.GET.get('type', None)
+        if course_type == 'free':
+            return Course.objects.filter(is_active=True, is_free=True).order_by('-created_at')
+        elif course_type == 'paid':
+            return Course.objects.filter(is_active=True, is_free=False).order_by('-created_at')
         return Course.objects.all().filter(is_active=True).order_by('-created_at')
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['course_type'] = self.request.GET.get('type', None)
+        return context
